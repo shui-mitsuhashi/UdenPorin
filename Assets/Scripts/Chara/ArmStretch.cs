aaee4e023c869bcb955bc7401fab3054f2cdbf50
shui-mitsuhashi <108126106+shui-mitsuhashi@users.noreply.github.com>
2024-11-19 11:29:19 +0900

    チェックアウトのため仮保存

--- Assets/Scripts/Chara/ArmStretch.cs
+++ Assets/Scripts/Chara/ArmStretch.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 public class ArmStretch : MonoBehaviour
 {
     public Transform leftArm;  // 左腕のTransform
-    public Transform rightArm; // 右腕のTransform
+    //public Transform rightArm; // 右腕のTransform
     public float stretchSpeed = 1.0f; // 腕が伸びる速度
     public float maxStretch = 2.0f; // 腕の最大の長さ
     public float originalScaleX = 1.0f; // 腕の元の長さ（X軸方向）
@@ -23,10 +23,10 @@ public class ArmStretch : MonoBehaviour
             }
 
             // 右腕が最大の長さに達するまで伸ばす
-            if (rightArm.localScale.x < maxStretch)
+            /*if (rightArm.localScale.x < maxStretch)
             {
                 rightArm.localScale += new Vector3(stretchSpeed * Time.deltaTime, 0, 0);
-            }
+            }*/
         }
         else if (Input.GetKeyUp(KeyCode.E)) // Eキーを離したら元に戻る
         {
@@ -34,7 +34,7 @@ public class ArmStretch : MonoBehaviour
             leftArm.localScale = new Vector3(originalScaleX, leftArm.localScale.y, leftArm.localScale.z);
 
             // 右腕の長さを元に戻す
-            rightArm.localScale = new Vector3(originalScaleX, rightArm.localScale.y, rightArm.localScale.z);
+            //rightArm.localScale = new Vector3(originalScaleX, rightArm.localScale.y, rightArm.localScale.z);
         }
     }
 }
