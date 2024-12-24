9488e63540072d5c550a21c85000f5289e47cbe4
urukun0615 <167834144+urukun0615@users.noreply.github.com>
2024-12-24 12:24:58 +0900

    title画面（仮）を反映

--- Assets/Scripts/TestUI/OptionMenu.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
-public class OptionMenu : MonoBehaviour
-{
-    public void ReturnToTitle()
-    {
-        SceneManager.LoadScene("TitleScene");
-    }
-}
